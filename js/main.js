b1fb4055667fb57716eb0d65d4019463a5c0119f
unknown <sarita-coder>
2016-05-21 08:07:48 -0500

    agregando

--- js/main.js
+++ js/main.js
@@ -1,25 +1,26 @@
-function maquina(contenedor,texto,intervalo){
-   // Calculamos la longitud del texto
-   longitud = texto.length;
-   // Obtenemos referencia del div donde se va a alojar el texto.
-   cnt = document.getElementById(contenedor);
-   var i=0;
-   // Creamos el timer
-   timer = setInterval(function(){
-      // Vamos añadiendo letra por letra y la _ al final.
-      cnt.innerHTML = cnt.innerHTML.substr(0,cnt.innerHTML.length-1) + texto.charAt(i) + "_";
-      // Si hemos llegado al final del texto..
-      if(i >= longitud){
-         // Salimos del Timer y quitamos la barra baja (_)
-         clearInterval(timer);
-         cnt.innerHTML = cnt.innerHTML.substr(0,longitud);
-         return true;
-      } else {
-         // En caso contrario.. seguimos
-         i++;
-      }},intervalo);
-};
+// function maquina(contenedor,texto,intervalo){
+//    // Calculamos la longitud del texto
+//    longitud = texto.length;
+//    // Obtenemos referencia del div donde se va a alojar el texto.
+//    cnt = document.getElementById(contenedor);
+//    var i=0;
+//    // Creamos el timer
+//    timer = setInterval(function(){
+//       // Vamos añadiendo letra por letra y la _ al final.
+//       cnt.innerHTML = cnt.innerHTML.substr(0,cnt.innerHTML.length-1) + texto.charAt(i) + "_";
+//       // Si hemos llegado al final del texto..
+//       if(i >= longitud){
+//          // Salimos del Timer y quitamos la barra baja (_)
+//          clearInterval(timer);
+//          cnt.innerHTML = cnt.innerHTML.substr(0,longitud);
+//          return true;
+//       } else {
+//          // En caso contrario.. seguimos
+//          i++;
+//       }
+//    },intervalo);
+// };
 
-var texto = "Desarrolladora Front-end, economista y estudiante de geomática, comprometida con los estudios y el trabajo, dispuesta a asumir retos y responsabilidades para convertirme en una “full-stack developer&quot;.";
-// 100 es el intervalo de minisegundos en el que se escribirá cada letra.
-maquina("maquina",texto,50);
+   // var texto = "Desarrolladora Front-end, economista y estudiante de geomática, comprometida con los estudios y el trabajo, dispuesta a asumir retos y responsabilidades para convertirme en una “full-stack developer&quot;.";
+   // // 100 es el intervalo de minisegundos en el que se escribirá cada letra.
+   // maquina("maquina",texto,50);
--- js/scroll.js
+++ js/scroll.js
@@ -2,21 +2,28 @@ $(document).scroll(function() {
     var scroll = $(this).scrollTop();
 
     if (scroll >= 100) {
-        $("#menu").addClass("active");
-    } else {
-        $("#menu").removeClass("active");
+        $("#menu").addClass("header");
+        $(".icon-bar").addClass("bar");
+        $(".icon-bar").removeClass("bar-inv");
+        $(".logo").addClass("logo-inver1");
+        $(".logo").removeClass("logo-inver");                
+        $(".link-header").addClass("inver");
+        $(".link-header").removeClass("inver1");
+
+    } else { 
+        $("#menu").removeClass("header");
+        $(".icon-bar").removeClass("bar");
+        $(".icon-bar").addClass("bar-inv");
+        $(".link-header").removeClass("inver");
+        $(".link-header").addClass("inver1");
+        $(".logo").removeClass("logo-inver1");
+        $(".logo").addClass("logo-inver");
+
     }
 
+
 });
-
-
 
 $(".navbar-toggle").click(function(){
   $(this).toggleClass("open");
 });
-//Menu
-/*$(".navbar-toggle").on('click', function() {
-  $(this).toggleClass("on");
-  $('.menu-section').toggleClass("on");
-  $("nav ul").toggleClass('hidden');
-});*/
