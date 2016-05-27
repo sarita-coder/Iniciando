aaf6605da5528e9ed56b26ca4303cd3a0491f66a
unknown <sarita-coder>
2016-05-27 18:50:38 -0500

    consejos de luisa a corregir

--- js/scroll.js
+++ js/scroll.js
@@ -24,3 +24,25 @@ $(document).scroll(function() {
 $(".navbar-toggle").click(function(){
   $(this).toggleClass("open");
 });
+$(document).ready(function() {
+    $(".link4").click(function() {
+        $("html, body").animate({scrollTop: $("#contacto").offset().top}, 2000);
+    });
+    $(".link1").click(function() {
+        $("html, body").animate({scrollTop: $("#Sobremi").offset().top}, 2000);
+    });
+    $(".link2").click(function() {
+        $("html, body").animate({scrollTop: $("#portafolio").offset().top}, 2000);
+    });
+    $(".link3").click(function() {
+        $("html, body").animate({scrollTop: $("#habilidades").offset().top}, 2000);
+    });
+    $(".navbar-brand").click(function() {
+        $("html, body").animate({scrollTop: $("#hero").offset().top}, 2000);
+    });
+});
+  
+
+
+
+
