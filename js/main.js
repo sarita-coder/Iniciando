0c6ed167499a197158f3f1c52146566f22b72aa4
sarita-coder <sarita.roman.colonio@gmail.com>
2016-06-05 11:34:17 -0500

    modificaciones en la presetacion contactame

--- js/main.js
+++ js/main.js
@@ -1,36 +1,6 @@
-<<<<<<< HEAD
-function maquina(contenedor,texto,intervalo){
-   // Calculamos la longitud del texto
-   longitud = texto.length;
-   // Obtenemos referencia del div donde se va a alojar el texto.
-   cnt = document.getElementById(contenedor);
-   var i=0;
-   // Creamos el timer
-   timer = setInterval(function(){
-      // Vamos añadiendo letra por letra y la _ al final.
-      cnt.innerHTML = cnt.innerHTML.substr(0,cnt.innerHTML.length-1) + texto.charAt(i) + "_";
-      // Si hemos llegado al final del texto..
-      if(i >= longitud){
-         // Salimos del Timer y quitamos la barra baja (_)
-         clearInterval(timer);
-         cnt.innerHTML = cnt.innerHTML.substr(0,longitud);
-         return true;
-      } else {
-         // En caso contrario.. seguimos
-         i++;
-      }
-   },intervalo);
-};
-
-var texto = "Programando con pasión y espíritu verde";
-// 100 es el intervalo de minisegundos en el que se escribirá cada letra.
-
-=======
->>>>>>> 44b9cd13e14d1c2e894f58979ebef998d6520565
-
  $(function(){
   	$('.portfolio_item').mixItUp();
-  	maquina("maquina",texto,150);
+
 });
 
 var desde=0,i=0,maximocaracteres=61,mensaje='Front-end, apasionada por el código y amante de la naturaleza. ';
